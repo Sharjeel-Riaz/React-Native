38165ccb74dfc45702885b4ef4086db851ca6e6e
Sharjeel Riaz <sharjeelriazsh@gmail.com>
2023-08-22 18:50:52 +0500

    Updated Chat-Screen

--- Projects/PaLMFlow/App/Pages/ChatScreen.js
+++ Projects/PaLMFlow/App/Pages/ChatScreen.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { StyleSheet } from "react-native";
 import { useRoute } from "@react-navigation/native";
-import { GiftedChat } from "react-native-gifted-chat";
+import { GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import GlobalApi from "../Services/GlobalApi";
 
 export default function ChatScreen() {
   const param = useRoute().params;
   const [messages, setMessages] = useState([]);
-  const [selectedChatFace, setSelectedChatFace] = useState();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setSelectedChatFace(param.selectedFace);
     setMessages([
       {
         _id: 1,
-        text: "Hello, I'm " + param.selectedFace?.name + ".",
+        text:
+          "Hello, I'm " + param.selectedFace?.name + ". How can I help you?",
         createdAt: new Date(),
         user: {
           _id: 2,
@@ -29,6 +30,8 @@ export default function ChatScreen() {
     setMessages((previousMessages) =>
       GiftedChat.append(previousMessages, messages)
     );
+
+    setLoading(true);
 
     if (messages[0].text) {
       getBardResp(messages[0].text);
@@ -51,14 +54,37 @@ export default function ChatScreen() {
         setMessages((previousMessages) =>
           GiftedChat.append(previousMessages, chatAPIResp)
         );
+        setLoading(false);
+      } else {
+        setLoading(false);
+        const chatAPIResp = {
+          _id: Math.random() * (9999999 - 1),
+          text: "Sorry, I can't help you with it.",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: param.selectedFace?.image,
+          },
+        };
+        setMessages((previousMessages) =>
+          GiftedChat.append(previousMessages, chatAPIResp)
+        );
       }
     });
   };
 
+  // Custom Input Toolbar
+  function renderInputToolbar(props) {
+    return <InputToolbar {...props} containerStyle={styles.toolbar} />;
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: "#FFF" }}>
       <GiftedChat
+        renderInputToolbar={renderInputToolbar}
         messages={messages}
+        isTyping={loading}
         onSend={(messages) => onSend(messages)}
         user={{
           _id: 1,
@@ -67,3 +93,16 @@ export default function ChatScreen() {
     </View>
   );
 }
+
+// Styles
+const styles = StyleSheet.create({
+  toolbar: {
+    borderRadius: 25,
+    backgroundColor: "#f0f0f0",
+    borderColor: "transparent",
+    marginLeft: 8,
+    marginRight: 8,
+    marginBottom: 5,
+    elevation: 1,
+  },
+});
