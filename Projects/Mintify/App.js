9bbb65fd19f46dd77b2368a47ba2971d5d551142
Sharjeel Riaz <sharjeelriazsh@gmail.com>
2023-09-16 06:00:52 +0500

    Added necessary dependencies

--- Projects/Mintify/App.js
+++ Projects/Mintify/App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>This is a NFT Marketplace.</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
