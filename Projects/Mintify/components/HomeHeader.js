9d198a6084e78096f62d85e1a67a86ef269cc1aa
Sharjeel Riaz <sharjeelriazsh@gmail.com>
2023-10-06 00:31:43 +0500

    Completed HomeHeader.js

--- Projects/Mintify/components/HomeHeader.js
+++ Projects/Mintify/components/HomeHeader.js
@@ -2,7 +2,7 @@ import { View, Text, Image, TextInput } from "react-native";
 
 import { COLORS, FONTS, SIZES, assets } from "../constants";
 
-const HomeHeader = () => {
+const HomeHeader = ({ onSearch }) => {
   return (
     <View
       style={{
@@ -73,24 +73,46 @@ const HomeHeader = () => {
             marginTop: SIZES.base / 2,
           }}
         >
-          Let's explore NFT's today!
+          Let's explore NFTs today!
         </Text>
       </View>
 
-      <View style={{
-        marginTop: SIZES.font,
-      }}>
-        <View style={{
-          width: "100%",
-          borderRadius: SIZES.font,
-          backgroundColor: COLORS.gray,
-          flexDirection: "row",
-          alignItems: "center",
-          paddingHorizontal: SIZES.padding,
-          paddingVertical: SIZES.small - 2,
-        }}>
-          <Image />
-          <TextInput />
+      <View
+        style={{
+          marginTop: SIZES.font,
+          marginBottom: SIZES.base,
+        }}
+      >
+        <View
+          style={{
+            width: "100%",
+            borderRadius: SIZES.font,
+            backgroundColor: COLORS.gray,
+            flexDirection: "row",
+            alignItems: "center",
+            paddingHorizontal: SIZES.padding,
+            paddingVertical: SIZES.small - 2,
+          }}
+        >
+          <Image
+            source={assets.search}
+            resizeMode="contain"
+            style={{
+              width: 20,
+              height: 20,
+              marginRight: SIZES.base,
+              marginLeft: SIZES.base,
+            }}
+          />
+          <TextInput
+            placeholder="Search NFTs"
+            placeholderTextColor={COLORS.white}
+            style={{
+              flex: 1,
+              color: COLORS.white,
+            }}
+            onChangeText={onSearch}
+          />
         </View>
       </View>
     </View>
